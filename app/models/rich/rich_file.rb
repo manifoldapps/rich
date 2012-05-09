9697c78b0b043541890d989c9979751f54f443bf
Benjamin Mort <ben@stringpane.com>
2012-05-09 14:53:07 +1000

    changed path again

--- app/models/rich/rich_file.rb
+++ app/models/rich/rich_file.rb
@@ -14,7 +14,7 @@ module Rich
                       :storage => Rich.file_storage,
                       :s3_host_alias => AWS_S3_BUCKET+'.s3.amazonaws.com',
                       :url => ':s3_alias_url',
-                      :path => "/rich/:style/:clean_file_name",
+                      :path => "/rich/:style/:id.:extension",
                       :s3_credentials => Rich.s3_credentials,
                       :s3_permissions => :public_read,
                       :styles => Proc.new {|a| a.instance.set_styles },
